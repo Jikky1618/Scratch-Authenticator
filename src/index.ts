a2cbd484cfd6952f9b2067b261a2e1d07da25e6e
Jikky <Jikky0110@gmail.com>
2024-03-14 01:38:42 +0900

    Update: 認証のメッセージを全て埋め込みに変更

--- src/index.ts
+++ src/index.ts
@@ -91,13 +91,23 @@ client.on("interactionCreate", async (i) => {
                 });
             } else {
                 i.reply({
-                    content: "このコマンドはテキストチャンネルでのみ使用できます。",
+                    content: "埋め込みのチャンネルはテキストチャンネルでのみ使用できます。",
                     ephemeral: true
                 });
             }
         }
     }
     if (i.isButton()) {
+        if (i.customId === "start") {
+            const modal = new ModalBuilder().setCustomId("auth").setTitle("scratch認証")
+            const username_input = new TextInputBuilder()
+                .setCustomId('username')
+                .setLabel("Scratchのユーザー名を入力してください")
+                .setStyle(TextInputStyle.Short);
+            const username_row = new ActionRowBuilder<TextInputBuilder>().addComponents(username_input);
+            modal.addComponents(username_row);
+            await i.showModal(modal);
+        }
         if (i.customId.startsWith("auth_")) {
             let uuid = i.customId.slice(5, 15), scratchId = i.customId.slice(16);
             await i.deferReply({ ephemeral: true });
@@ -110,39 +120,34 @@ client.on("interactionCreate", async (i) => {
                 elem.value === uuid &&
                 elem.user === scratchId
             )) {
-                i.followUp("認証が完了しました。");
+                const embed = new EmbedBuilder()
+                    .setTitle("認証完了")
+                    .setDescription("認証が完了しました。")
+                    .setColor("Green");
+                i.editReply({ embeds: [embed] });
                 s.curator("31009600", scratchId);
-                if (i.channel?.isTextBased()) {
-                    (i.member?.roles as GuildMemberRoleManager).add(process.env.ROLE_ID as string);
-                    const embed = new EmbedBuilder()
-                        .setTitle("認証完了")
-                        .setColor("Green")
-                        .addFields(
-                            { name: "scratchユーザ名", value: `[${scratchId}](https://scratch.mit.edu/users/${scratchId})` },
-                            { name: "Discordユーザ名", value: `${i.user?.tag}(<@!${i.user?.id}>)` },
-                            { name: "DiscordユーザID", value: i.user?.id },
-                            { name: "検証用ID", value: uuid },
-                            { name: "認証日時", value: new Date().toLocaleString() },
-                        )
-                    const channel = i.guild?.channels.cache.get(process.env.LOG_CHANNEL_ID as string) as TextChannel;
-                    if (channel && channel.isTextBased()) {
-                        channel.send({ embeds: [embed] });
-                    }
+                (i.member?.roles as GuildMemberRoleManager).add(process.env.ROLE_ID as string);
+                const embed_log = new EmbedBuilder()
+                    .setTitle("認証完了")
+                    .setColor("Green")
+                    .addFields(
+                        { name: "scratchユーザ名", value: `[${scratchId}](https://scratch.mit.edu/users/${scratchId})` },
+                        { name: "Discordユーザ名", value: `${i.user?.tag}(<@!${i.user?.id}>)` },
+                        { name: "DiscordユーザID", value: i.user?.id },
+                        { name: "検証用ID", value: uuid },
+                        { name: "認証日時", value: new Date().toLocaleString() },
+                    )
+                const channel = i.guild?.channels.cache.get(process.env.LOG_CHANNEL_ID as string) as TextChannel;
+                if (channel && channel.isTextBased()) {
+                    channel.send({ embeds: [embed_log] });
                 }
             } else {
-                console.log(data, uuid, scratchId);
-                i.followUp("認証に失敗しました。ユーザー名が正しいかどうかを確認してください。");
+                const embed = new EmbedBuilder()
+                    .setTitle("認証失敗")
+                    .setDescription("認証に失敗しました。")
+                    .setColor("Red");
+                i.editReply({ embeds: [embed] });
             }
-        }
-        if (i.customId === "start") {
-            const modal = new ModalBuilder().setCustomId("auth").setTitle("scratch認証")
-            const username_input = new TextInputBuilder()
-                .setCustomId('username')
-                .setLabel("Scratchのユーザー名を入力してください")
-                .setStyle(TextInputStyle.Short);
-            const username_row = new ActionRowBuilder<TextInputBuilder>().addComponents(username_input);
-            modal.addComponents(username_row);
-            await i.showModal(modal);
         }
     }
     if (i.isModalSubmit()) {
@@ -156,24 +161,22 @@ client.on("interactionCreate", async (i) => {
                 method: "get",
             }).then(() => {
                 const embed = new EmbedBuilder()
-                    .setTitle("認証コード")
-                    .setDescription(`\`\`\`\n${uuid}\n\`\`\``)
+                    .setTitle("認証コードを入力")
+                    .setDescription(`ユーザー名の確認ができました。\n次に、下のコード(\`XXXXXXXXX\`形式)を、 [このプロジェクト](https://scratch.mit.edu/projects/${process.env.PROJECT_ID}/fullscreen/)に入力してください。`)
+                    .addFields({ name: "認証コード", value: `\`\`\`\n${uuid}\n\`\`\`` })
                     .setColor("Green");
                 const auth = new ButtonBuilder()
                     .setCustomId("auth_" + uuid + "_" + scratchId)
                     .setLabel("プロジェクトに入力しました")
                     .setStyle(ButtonStyle.Success);
                 const row = new ActionRowBuilder<ButtonBuilder>().addComponents(auth);
-                i.editReply({
-                    content: `ユーザー名の確認ができました。\n次に、下のコード(\`XXXXXXXXX\`形式)を、 https://scratch.mit.edu/projects/${process.env.PROJECT_ID}/fullscreen/ に入力してください。`,
-                    embeds: [embed],
-                    components: [row],
-                });
-            }).catch((e) => {
-                i.reply({
-                    ephemeral: true,
-                    content: "有効なユーザー名を入力してください"
-                })
+                i.editReply({ embeds: [embed], components: [row] });
+            }).catch(() => {
+                const embed = new EmbedBuilder()
+                    .setTitle("エラー発生")
+                    .setDescription("有効なユーザー名を入力してください。")
+                    .setColor("Red");
+                i.editReply({ embeds: [embed] });
             });
         }
     }
@@ -182,10 +185,10 @@ client.on("interactionCreate", async (i) => {
 const s = new scratch(String(process.env.SCRATCH_USER_NAME), String(process.env.SCRATCH_USER_PASSWORD));
 s.on("login", async () => {
     try {
-        if ((await fs.stat(path.join(__dirname,"../data/"))).isDirectory()){
-            
+        if ((await fs.stat(path.join(__dirname, "../data/"))).isDirectory()) {
+
         }
-    }catch(e){
+    } catch (e) {
     }
-        client.login(process.env.TOKEN);
+    client.login(process.env.TOKEN);
 })
